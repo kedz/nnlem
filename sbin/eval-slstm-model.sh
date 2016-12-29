79d24d6d51604b61a03e1210fd8400ee35eadd54
Chris Kedzie <kedzie@cs.columbia.edu>
2016-12-29 13:19:39 -0800

    Eval multiple slstm models at a time using gnu parallel.

--- sbin/eval-slstm-model.sh
+++ sbin/eval-slstm-model.sh
@@ -1,25 +1,26 @@
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd`
-popd > /dev/null
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTPATH/..
 BASEPATH=`pwd`
-cd $SCRIPTPATH
 
 DATAPATH=$BASEPATH/data
 TRAINPATH=$DATAPATH/lemmas.en.train.txt
 DEVPATH=$DATAPATH/lemmas.en.dev.txt
 TESTPATH=$DATAPATH/lemmas.en.test.txt
 
-LUA_PATH_OLD=$LUA_PATH
-LUA_PATH="$LUA_PATH;$BASEPATH/lua/?.lua"
+MODELS=`ls $BASEPATH/models | grep slstm`
 
-th $BASEPATH/lua/eval-model.lua \
-    --model $BASEPATH/slstm-models \
+mkdir -p $BASEPATH/results
+
+parallel -j4 --progress --results /tmp/slstm-ev/ --joblog /tmp/slstm-ev-log \
+    th $BASEPATH/lua/eval-model.lua \
+    --results $BASEPATH/results/{1}.tsv \
+    --model $BASEPATH/models/{1} \
     --vocab $DATAPATH/vocab.txt \
     --train-data $TRAINPATH \
     --dev-data $DEVPATH \
-    --batch-size 15 \
+    --batch-size 50 \
     --start-epoch 1 \
     --stop-epoch 50 \
     --seed 1986 \
-    --gpu 0
+    --progress false \
+    --gpu 0 ::: $MODELS
