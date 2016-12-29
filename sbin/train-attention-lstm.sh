0f29a77be9d08764f160677938a3ce5134af9556
Chris Kedzie <kedzie@cs.columbia.edu>
2016-12-29 13:36:46 -0800

    Train multiple hyperparameter settings with gnu parallel.

--- sbin/train-attention-lstm.sh
+++ sbin/train-attention-lstm.sh
@@ -1,19 +1,26 @@
-SCRIPTPATH=`pwd`
-popd > /dev/null
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTPATH/..
 BASEPATH=`pwd`
-cd $SCRIPTPATH
 
 DATAPATH=$BASEPATH/data
 TRAINPATH=$DATAPATH/lemmas.en.train.txt
 DEVPATH=$DATAPATH/lemmas.en.dev.txt
 TESTPATH=$DATAPATH/lemmas.en.test.txt
 
-LUA_PATH_OLD=$LUA_PATH
-LUA_PATH="$LUA_PATH;$BASEPATH/lua/?.lua"
+DIMS="256 128 64 32"
+LAYERS="3 2 1"
+LRS="1E-3 1E-2 1E-4"
+BSS="15 25 50"
 
-th $BASEPATH/lua/train-attention-lstm.lua --data $TRAINPATH \
-    --dims 64 \
-    --vocab $DATAPATH/vocab.txt --layers 1 \
-    --save $BASEPATH/alstm-models
+parallel -j4 --progress --results /tmp/alstm/ --joblog /tmp/alstm-log \
+    th $BASEPATH/lua/train-attention-lstm.lua \
+    --vocab $DATAPATH/vocab.txt \
+    --data $TRAINPATH \
+    --dims {1} \
+    --layers {2} \
+    --lr {3} \
+    --batch-size {4} \
+    --save $BASEPATH/models/alstm.dim{1}.layers{2}.lr{3}.bs{4} \
+    --progress false ::: $DIMS ::: $LAYERS ::: $LRS ::: $BSS
+    
 
