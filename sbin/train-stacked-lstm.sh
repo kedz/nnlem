cbd657493ba43370c4b5a7d83a3fbb2ef8f4f14c
Chris Kedzie <kedzie@cs.columbia.edu>
2016-12-29 11:16:48 -0800

    Added hyperparameter search with gnu parallel.

--- sbin/train-stacked-lstm.sh
+++ sbin/train-stacked-lstm.sh
@@ -1,23 +1,26 @@
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd`
-popd > /dev/null
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTPATH/..
 BASEPATH=`pwd`
-cd $SCRIPTPATH
 
 DATAPATH=$BASEPATH/data
 TRAINPATH=$DATAPATH/lemmas.en.train.txt
 DEVPATH=$DATAPATH/lemmas.en.dev.txt
 TESTPATH=$DATAPATH/lemmas.en.test.txt
 
+DIMS="256 128 64 32"
+LAYERS="3 2 1"
+LRS="1E-3 1E-2 1E-4"
+BSS="15 25 50"
 
-echo "$BASEPATH !!!"
-
-LUA_PATH_OLD=$LUA_PATH
-LUA_PATH="$LUA_PATH;$BASEPATH/lua/?.lua"
-
-th $BASEPATH/lua/train-stacked-lstm.lua --data $TRAINPATH \
-    --dims 64 \
-    --vocab $DATAPATH/vocab.txt --layers 1 \
-    --save $BASEPATH/slstm-models
+parallel -j4 --progress --results /tmp/slstm/ --joblog /tmp/slstm-log \
+    th $BASEPATH/lua/train-stacked-lstm.lua \
+    --vocab $DATAPATH/vocab.txt \
+    --data $TRAINPATH \
+    --dims {1} \
+    --layers {2} \
+    --lr {3} \
+    --batch-size {4} \
+    --save $BASEPATH/models/slstm.dim{1}.layers{2}.lr{3}.bs{4} \
+    --progress false ::: $DIMS ::: $LAYERS ::: $LRS ::: $BSS
+    
 
